22851b135ba8426cefdcd55ba82a2e59aab997cf
a8trejo <abel130@hotmail.com>
2022-09-15 10:13:58 -0400

    Commenting workflow steps for debugging

--- cypress/e2e/HeroApp.feature
+++ cypress/e2e/HeroApp.feature
@@ -4,6 +4,6 @@ Background: Authorized access
     Given I open the 'HeroApp' demo page
 
 Scenario: Control Appium WDIO with node
-    When I open the 'Playground' app in the Android 'DemoAVD' device
-    Then I can run a test 'tabs and input' on the 'Playground' app with WDIO
-    # And I can come back to the browser
+    # When I open the 'Playground' app in the Android 'DemoAVD' device
+    # Then I can run a test 'tabs and input' on the 'Playground' app with WDIO
+    And I can come back to the browser
--- cypress/e2e/HeroApp/HeroAppSteps.js
+++ cypress/e2e/HeroApp/HeroAppSteps.js
@@ -4,6 +4,9 @@ import { Given, When, Then, And } from "@badeball/cypress-cucumber-preprocessor"
 import HeroAppQuery from '../../support/Selectors/HeroAppQuery';
 import HeroAppObject from './HeroAppObject'
 
+const baseURL = Cypress.env("HeroApp")
+Cypress.config('baseUrl', baseURL)
+
 before(() => {
     cy.determineOs()
     cy.stopAppiumServer()
@@ -22,4 +25,9 @@ When('I click on the {string} demo option', (option) => {
     }
 
     HeroAppQuery.selectors(option).click()
+})
+
+// And I can come back to the browser
+And('I can come back to the browser', () => {
+    cy.visit('/')
 })
